c00b051ff819a2405f406c5ac8938ad4d0ba6682
Alexandre Augusto Fonseca <alexandralexandrefonsecach@gmail.com>
2023-11-24 21:34:44 -0300

    fix Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-purple-700 text-white flex flex-col justify-center items-center h-16 bottom-0 w-full object-fit mt-10">
+    <footer className="bg-purple-700 text-white flex flex-col justify-center items-center h-16 bottom-0 w-full object-fit mt-10 p-12">
       <p className="text-lg font-bold">© 2023 AnimeWiki</p>
       <p className="text-lg font-bold">Powered by Alexandre Fonseca</p>
     </footer>
