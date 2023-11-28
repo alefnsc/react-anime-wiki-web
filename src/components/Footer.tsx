ed0abdf9a1a2f9f427f37b5a90469cdbda0c1fcb
Alexandre Augusto Fonseca <alexandralexandrefonsecach@gmail.com>
2023-11-28 19:10:39 -0300

    fix footer responsivity

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
 export default function Footer() {
   return (
     <footer className="bg-purple-700 text-white flex flex-col justify-center items-center h-16 bottom-0 w-full object-fit mt-10 p-12">
-      <p className="text-lg font-bold">© 2023 AnimeWiki</p>
-      <p className="text-lg font-bold">Powered by Alexandre Fonseca</p>
+      <p className="md:text-l lg:text-xl sm-text-md font-bold">
+        © 2023 AnimeWiki
+      </p>
+      <p className=" md:text-l lg:text-xl sm-text-md font-bold">
+        Powered by Alexandre Fonseca
+      </p>
     </footer>
   );
 }
